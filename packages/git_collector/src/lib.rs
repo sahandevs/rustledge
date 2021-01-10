d61a8f37aebb4ff32ae02e169311badbf43678c8
Sahandevs <sahandevs@gmail.com>
2021-01-10 19:52:45 +0330

    add get_test_repo util test func

--- packages/git_collector/src/lib.rs
+++ packages/git_collector/src/lib.rs
@@ -9,7 +9,7 @@ const COMMIT_DESCRIPTION: &str = "COMMIT-DESCRIPTION";
 const FILES: &str = "FILES";
 
 fn test() -> Result<(), git2::Error> {
-    let repo = Repository::init("./test_git_repos")?;
+    let repo = Repository::init("./test_artifacts")?;
     repo.head().unwrap();
     let desc = repo.describe(&DescribeOptions::new())?;
     println!("{}", desc.format(None)?);
@@ -31,9 +31,6 @@ pub fn create_bucket_from_head(repository: &Repository) -> Result<bucket::Bucket
     bucket.set(COMMIT_DESCRIPTION, bucket::Value::String(commit_description));
     bucket.set(IS_HEAD, bucket::Value::Bool(true));
     let mut files_bucket = bucket::Bucket::new();
-
-    fs::read_dir()
-
     files_bucket.set("file.txt", bucket::Value::String("test string file".to_string()));
     files_bucket.set("folder/file", bucket::Value::String("file2 content".to_string()));
     bucket.set(FILES, bucket::Value::Bucket(files_bucket));
@@ -44,10 +41,24 @@ pub fn create_bucket_from_head(repository: &Repository) -> Result<bucket::Bucket
 mod tests {
     use crate::*;
     use bucket;
+    use zip;
+    use std::path;
+    use serial_test::serial;
+
+    fn get_test_repo() -> Repository {
+        let file = fs::File::open("./test_repo.zip").expect("test_repo.zip archive is missing!");
+        let mut archive = zip::ZipArchive::new(file).unwrap();
+        fs::remove_dir("./test_artifacts/test_repo").unwrap_or_default();
+        fs::create_dir_all("./test_artifacts/test_repo").unwrap();
+        let repo_path = path::Path::new("./test_artifacts/test_repo");
+        archive.extract(repo_path).unwrap();
+        Repository::open(repo_path).unwrap()
+    }
 
     #[test]
+    #[serial(TestRepo)]
     fn it_works() -> Result<(), git2::Error> {
-        let repo = Repository::init("./test_git_repos/test1")?;
+        let repo = get_test_repo();
 
         let result = create_bucket_from_head(&repo)?;
 
