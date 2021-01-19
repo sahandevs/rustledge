904dc613a8c08b671baa900ce463f47f6920080f
Sahandevs <sahandevs@gmail.com>
2021-01-19 14:35:39 +0330

    implement "env:*: values for config

--- packages/rustledge/src/config.rs
+++ packages/rustledge/src/config.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 use serde_json;
-use std::fs;
+use std::{fs, env};
 use serde::{Deserialize};
 
 #[derive(Deserialize)]
@@ -22,6 +22,22 @@ pub struct ApiConfig {
 
 pub fn read_config(path: &Path) -> Config {
     let raw_config_str = fs::read_to_string(path).unwrap();
-    let config: Config = serde_json::from_str(&raw_config_str).unwrap();
+    let mut config: Config = serde_json::from_str(&raw_config_str).unwrap();
+    process_config(&mut config);
     config
+}
+
+#[inline]
+fn _replace_string_with_env(str: &mut String) {
+    if str.starts_with("env:") {
+        *str = env::var(str.replace("env:", "")).unwrap();
+    }
+}
+
+/// replace env:VARIABLE values with value from environment variables
+fn process_config(config: &mut Config) {
+    for repo in &mut config.git_repos {
+        _replace_string_with_env(repo);
+    }
+    _replace_string_with_env(&mut config.index_server.db_path);
 }
